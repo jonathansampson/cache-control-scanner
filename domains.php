03a0a2d5e26396d5ce7aa52274b3db873e5f07a4
Jonathan Sampson <jjdsampson@gmail.com>
2013-12-14 21:22:02 -0200

    Domain list acquired from Wikipedia

---
+++ domains.php
@@ -0,0 +1,28 @@
+<?php
+
+	/* 
+		Collection of domains from http://en.wikipedia.org/wiki/List_of_most_popular_websites 
+		Lifted from page using jQuery in the Browser's Console:
+
+		$(".wikitable tr:gt(0) td:nth-child(2)").contents();
+
+	*/
+	$domains = [
+		"google.com", "facebook.com", "youtube.com", "yahoo.com", "baidu.com", "wikipedia.org", "live.com", 
+		"amazon.com", "qq.com", "twitter.com", "taobao.com", "blogspot.com", "google.co.in", "linkedin.com", 
+		"yahoo.co.jp", "sina.com.cn", "msn.com", "yandex.ru", "ebay.com", "google.co.jp", "google.de", 
+		"wordpress.com", "bing.com", "google.com.hk", "vk.com", "google.co.uk", "google.fr", "babylon.com", 
+		"163.com", "weibo.com", "microsoft.com", "tumblr.com", "googleusercontent.com", "mail.ru", 
+		"pinterest.com", "apple.com", "google.com.br", "soso.com", "PayPal.com", "google.ru", "google.es", 
+		"google.it", "xhamster.com", "tmall.com", "fc2.com", "blogger.com", "imdb.com", "craigslist.org", 
+		"xvideos.com", "ask.com", "sohu.com", "hao123.com", "conduit.com", "bbc.co.uk", "go.com", 
+		"amazon.co.jp", "google.com.mx", "odnoklassniki.ru", "google.ca", "youku.com", "livejasmin.com", 
+		"amazon.de", "adobe.com", "flickr.com", "avg.com", "ifeng.com", "t.co", "pornhub.com", "aol.com", 
+		"rakuten.co.jp", "cnn.com", "thepiratebay.sx", "mywebsearch.com", "ebay.de", "amazon.co.uk", 
+		"adf.ly", "alibaba.com", "espn.go.com", "blogspot.in", "google.com.tr", "redtube.com", 
+		"google.co.id", "instagram.com", "huffingtonpost.com", "alipay.com", "about.com", 
+		"stackoverflow.com", "sogou.com", "google.com.au", "livedoor.com", "360buy.com", "ebay.co.uk", 
+		"netflix.com", "dailymotion.com", "imgur.com", "zedo.com", "google.pl", "uol.com.br", "360.cn"
+	];
+
+?>
